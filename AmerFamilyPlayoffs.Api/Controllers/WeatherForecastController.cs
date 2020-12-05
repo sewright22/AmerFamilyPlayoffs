9913f88f2141e6d6c5e43a9f1048f61f886a51e9
sewright22 <sewright22@gmail.com>
2020-12-05 17:10:20 -0600

    Updated API publish

--- AmerFamilyPlayoffs.Api/Controllers/WeatherForecastController.cs
+++ AmerFamilyPlayoffs.Api/Controllers/WeatherForecastController.cs
@@ -30,7 +30,7 @@
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
+                TemperatureC = rng.Next(56, 100),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
