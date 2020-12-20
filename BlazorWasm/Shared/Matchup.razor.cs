3613374e3c9d74a1e3e0161280cedd5c0f19e745
sewright22 <sewright22@gmail.com>
2020-12-20 11:33:54 -0600

    Created Matchup partial view.

---
+++ BlazorWasm/Shared/Matchup.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace AmerFamilyPlayoffs.Shared
+{
+    using AmerFamilyPlayoffs.Models;
+    using Microsoft.AspNetCore.Components;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public partial class Matchup : ComponentBase
+    {
+        [Parameter]
+        public GameModel Game { get; set; }
+    }
+}
