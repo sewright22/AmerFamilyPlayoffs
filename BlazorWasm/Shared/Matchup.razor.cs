1c9557c63b864ff39aacd98a1e8bb908efc9eaec
sewright22 <sewright22@gmail.com>
2020-12-20 17:22:09 -0600

    It now auto unchecks the other losing team.

--- BlazorWasm/Shared/Matchup.razor.cs
+++ BlazorWasm/Shared/Matchup.razor.cs
@@ -9,7 +9,71 @@
 
     public partial class Matchup : ComponentBase
     {
+        private bool homeIsWinner;
+        private bool awayIsWinner;
+
         [Parameter]
         public GameModel Game { get; set; }
+
+        public bool HomeIsWinner
+        {
+            get
+            {
+                return this.homeIsWinner;
+            }
+
+            set
+            {
+                if (this.homeIsWinner != value)
+                {
+                    this.homeIsWinner = value;
+
+                    if (this.homeIsWinner)
+                    {
+                        AwayIsWinner = false;
+                    }
+
+                    UpdateWinner();
+                }
+            }
+        }
+
+        public bool AwayIsWinner
+        {
+            get
+            {
+                return this.awayIsWinner;
+            }
+            set
+            {
+                if (this.awayIsWinner != value)
+                {
+                    this.awayIsWinner = value;
+
+                    if (this.awayIsWinner)
+                    {
+                        HomeIsWinner = false;
+                    }
+
+                    UpdateWinner();
+                }
+            }
+        }
+
+        public void UpdateWinner()
+        {
+            if (AwayIsWinner)
+            {
+                Game.Winner = Game.AwayTeam;
+            }
+            else if (HomeIsWinner)
+            {
+                Game.Winner = Game.HomeTeam;
+            }
+            else
+            {
+                Game.Winner = null;
+            }
+        }
     }
 }
