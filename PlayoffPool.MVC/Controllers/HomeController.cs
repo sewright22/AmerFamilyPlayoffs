f03a9640d3b7197137dd23282c9764120af30fbd
sewright22 <sewright22@gmail.com>
2022-11-21 21:01:33 -0600

    Added MVC project.

---
+++ PlayoffPool.MVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using AmerFamilyPlayoffs.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlayoffPool.MVC.Models;
+
+namespace PlayoffPool.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AmerFamilyPlayoffContext dataContext;
+
+        public HomeController(ILogger<HomeController> logger, AmerFamilyPlayoffContext dataContext)
+        {
+            _logger = logger;
+            this.dataContext = dataContext;
+            this.dataContext.Database.Migrate();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
