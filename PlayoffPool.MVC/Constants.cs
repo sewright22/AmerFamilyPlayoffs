d618a400d1f2f61eda568cc3ba4a5ac85b6cfba5
gracie.amer <gracie.amer@gmail.com>
2022-11-25 11:48:09 -0600

    Added Logut to Constants file.

--- PlayoffPool.MVC/Constants.cs
+++ PlayoffPool.MVC/Constants.cs
@@ -8,11 +8,13 @@ namespace PlayoffPool.MVC
 		public static class Controllers
 		{
 			public static string ACCOUNT = nameof(AccountController).GetControllerNameForUri();
+			public static string HOME = nameof(HomeController).GetControllerNameForUri();
 		}
 
 		public static class Actions
 		{
 			public static string LOGIN = nameof(AccountController.Login);
+			public static string LOGOUT = nameof(HomeController.LogOut);
 			public static string REGISTER = nameof(AccountController.Register);
 		}
 	}
