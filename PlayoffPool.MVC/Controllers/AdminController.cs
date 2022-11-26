8b545a1329bc2d43938d4b4726ee2eb04278c6db
sewright22 <sewright22@gmail.com>
2022-11-26 11:34:30 -0600

    WIP user management

--- PlayoffPool.MVC/Controllers/AdminController.cs
+++ PlayoffPool.MVC/Controllers/AdminController.cs
@@ -7,7 +7,6 @@
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.EntityFrameworkCore;
     using PlayoffPool.MVC.Models;
-    using PlayoffPool.MVC.Models.Admin;
     using System;
 
     public class AdminController : Controller
