dbf782ff184d124d6683da51e7ade3970a23a993
sewright22 <sewright22@gmail.com>
2022-12-13 19:46:42 -0600

    Working on Bracket View

---
+++ PlayoffPool.MVC/Controllers/BracketController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlayoffPool.MVC.Models.Bracket;
+
+namespace PlayoffPool.MVC.Controllers
+{
+    public class BracketController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(int? bracketId)
+        {
+            var model = new BracketViewModel();
+
+            if (bracketId is not null)
+            {
+                // get bracket info from database.
+            }
+
+            model.Name = "Test";
+            model.CanEdit = true;
+
+            return View(model);
+        }
+    }
+}
---
+++ PlayoffPool.MVC/Models/Bracket/BracketViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlayoffPool.MVC.Models.Bracket
+{
+    public class BracketViewModel
+    {
+        [Display(Name = "Bracket Name", Prompt = "Please name your bracket")]
+        [Required]
+        public string? Name { get; set; }
+
+        public bool CanEdit { get; set; }
+    }
+}
